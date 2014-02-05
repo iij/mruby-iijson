a593caf06c8858db47f8deb5db20790c6472cb63
Tomoyuki Sahara <tsahara@iij.ad.jp>
2014-02-05 16:52:14 +0900

    import.

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
-mruby-iijson
-============
+# mruby-json
 
-JSON module for mruby
+"mruby-json" is JSON module for mruby.
+
+## Implemented Methods:
+ - dump, generate, load, parse
+
+
+## Example:
+
+```Ruby
+str = '{
+  "Image": {
+    "Width":  800,
+    "Height": 600,
+    "Title":  "View from 15th Floor",
+    "Thumbnail": {
+      "Url":    "http://www.example.com/image/481989943",
+      "Height": 125,
+      "Width":  "100"
+    },
+    "IDs": [116, 943, 234, 38793]
+  }
+}'
+p JSON.parse(str)
+
+h = {
+  :precision => "zip",
+  :Latitude  =>  37.7668,
+  :Longitude => -122.3959,
+  :Address   => "",
+  :City      => "SAN FRANCISCO",
+  :State     => "CA",
+  :Zip       => "94107",
+  :Country   => "US"
+}
+p JSON.generate(h)
+```
+
+```
+{"Image"=>{"Thumbnail"=>{"Url"=>"http://www.example.com/image/481989943", "Height"=>125, "Width"=>"100"}, "Title"=>"View from 15th Floor", "Height"=>600, "Width"=>800, "IDs"=>[116, 943, 234, 38793]}}
+"{\"Longitude\":-122.3959,\"Address\":\"\",\"City\":\"SAN FRANCISCO\",\"State\":\"CA\",\"precision\":\"zip\",\"Latitude\":37.7668,\"Zip\":\"94107\",\"Country\":\"US\"}"
+```
+
+
+## Caveats
+
+ - JSON.generate won't return if input Array/Hash has circular reference.
+
+
+## License
+
+Copyright (c) 2014 Internet Initiative Japan Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
---
+++ src/json.c
@@ -0,0 +1,536 @@
+#include <ctype.h>
+#include <string.h>
+
+#include "mruby.h"
+#include "mruby/array.h"
+#include "mruby/hash.h"
+#include "mruby/string.h"
+
+#define E_JSON_PARSER_ERROR (mrb_class_get_under(mrb, mrb_class_get(mrb, "JSON"), "ParserError"))
+
+#define JSON_EOF -1
+
+#define DEFAULT_MAX_NESTING 100
+
+struct json_parser {
+  mrb_state *mrb;
+  mrb_value src;
+  unsigned int cursor;
+  unsigned int lineno;
+  int nesting;
+
+  int max_nesting;
+};
+
+static void json_check_nesting(struct json_parser *);
+static int json_delimiter_p(char ch);
+static int json_getc(struct json_parser *);
+static void json_skip_ws(struct json_parser *);
+static void json_ungetc(struct json_parser *);
+static int json_whitespace_p(char ch);
+
+static int json_parse_array(struct json_parser *, mrb_value *);
+static int json_parse_object(struct json_parser *, mrb_value *);
+static int json_parse_number2(struct json_parser *, int, mrb_value *, int, int);
+static int json_parse_string(struct json_parser *, mrb_value *);
+static int json_parse_value(struct json_parser *, mrb_value *);
+
+static void
+json_check_nesting(struct json_parser *parser)
+{
+  mrb_state *mrb = parser->mrb;
+
+  if (parser->max_nesting != -1) {
+    if (parser->nesting >= parser->max_nesting) {
+      // +1 for compatibility with CRuby
+      mrb_raisef(mrb, E_JSON_PARSER_ERROR, "nesting of %S is too deep", mrb_fixnum_value(parser->nesting+1));
+    }
+  }
+}
+
+static int
+json_delimiter_p(char ch)
+{
+  return (json_whitespace_p(ch) || ch == ',' || ch == ']' || ch == '}');
+}
+
+static int
+json_getc(struct json_parser *parser)
+{
+  if (parser->cursor < RSTRING_LEN(parser->src)) {
+    unsigned char ch = RSTRING_PTR(parser->src)[parser->cursor];
+    parser->cursor++;
+    return ch;
+  } else {
+    return JSON_EOF;
+  }
+}
+
+static int
+json_parse_readstring(struct json_parser *parser, const char *str)
+{
+  size_t len;
+  int ch;
+
+  len = strlen(str);
+  if (parser->cursor + len > RSTRING_LEN(parser->src))
+    return 0;
+  if (memcmp(str, RSTRING_PTR(parser->src) + parser->cursor, len) != 0)
+    return -1;
+  parser->cursor += len;
+  if (parser->cursor == RSTRING_LEN(parser->src))
+    return 1;
+  ch = RSTRING_PTR(parser->src)[parser->cursor];
+  if (!json_delimiter_p(ch))
+    return -1;
+  return 1;
+}
+
+static void
+json_skip_ws(struct json_parser *parser)
+{
+  int ch;
+
+  do {
+    ch = json_getc(parser);
+    if (ch == 0x0a)
+      parser->lineno++;
+  } while (json_whitespace_p(ch));
+  if (ch != JSON_EOF) {
+    json_ungetc(parser);
+  }
+}
+
+static void
+json_ungetc(struct json_parser *parser)
+{
+  if (parser->cursor > 0)
+    parser->cursor--;
+}
+
+static int
+json_whitespace_p(char ch)
+{
+  return (ch == 0x20 || ch == 0x09 || ch == 0x0a || ch == 0x0d);
+}
+
+static int
+json_parse_array(struct json_parser *parser, mrb_value *result)
+{
+  mrb_state *mrb = parser->mrb;
+  mrb_value ary, v;
+  int ch;
+
+  json_check_nesting(parser);
+
+  ary = mrb_ary_new(mrb);
+
+  json_skip_ws(parser);
+  ch = json_getc(parser);
+  if (ch == ']') { /* easy case */
+    *result = ary;
+    return 1;
+  }
+  if (ch == JSON_EOF) {
+    mrb_raise(mrb, E_JSON_PARSER_ERROR, "JSON_EOF in array(1)");
+  }
+  json_ungetc(parser);
+
+  while (1) {
+    parser->nesting++;
+    if (json_parse_value(parser, &v) != 1) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "error in array");
+    }
+    parser->nesting--;
+
+    mrb_ary_push(mrb, ary, v);
+
+    json_skip_ws(parser);
+    ch = json_getc(parser);
+    if (ch == ']') {
+      break;
+    }
+    if (ch == JSON_EOF) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "JSON_EOF in array(2)");
+    }
+    if (ch != ',') {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "not ',' in array");
+    }
+  }
+  *result = ary;
+  return 1;
+}
+
+static int
+json_parse_object(struct json_parser *parser, mrb_value *result)
+{
+  mrb_state *mrb = parser->mrb;
+  mrb_value h, k, v;
+  int ch;
+
+  json_check_nesting(parser);
+
+  h = mrb_hash_new(mrb);
+
+  json_skip_ws(parser);
+  ch = json_getc(parser);
+  if (ch == '}') { /* easy case */
+    *result = h;
+    return 1;
+  }
+  if (ch == JSON_EOF) {
+    mrb_raise(mrb, E_JSON_PARSER_ERROR, "EOF in object(1)");
+  }
+  json_ungetc(parser);
+
+  while (1) {
+    parser->nesting++;
+    if (json_parse_value(parser, &k) != 1) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "error in object key");
+    }
+    parser->nesting--;
+    if (! mrb_string_p(k)) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "key is not a string");
+    }
+
+    json_skip_ws(parser);
+
+    ch = json_getc(parser);
+    if (ch == JSON_EOF) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "EOF in object(2)");
+    }
+    if (ch != ':') {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "no ':' in object");
+    }
+
+    parser->nesting++;
+    if (json_parse_value(parser, &v) != 1) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "error in object value");
+    }
+    parser->nesting--;
+
+    mrb_hash_set(mrb, h, k, v);
+
+    json_skip_ws(parser);
+    ch = json_getc(parser);
+    if (ch == '}') {
+      break;
+    }
+    if (ch == JSON_EOF) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "EOF in object(3)");
+    }
+    if (ch != ',') {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "no ',' in object");
+    }
+  }
+  *result = h;
+  return 1;
+}
+
+static int
+json_parse_number(struct json_parser *parser, int ch, mrb_value *result)
+{
+  mrb_state *mrb = parser->mrb;
+  mrb_int num;
+  int d, sign;
+
+  if (ch == '-') {
+    sign = -1;
+    ch = json_getc(parser);
+    if (ch == JSON_EOF) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "no character following minus");
+    }
+    if (!isdigit(ch)) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "character following minus is not a digit");
+    }
+  } else {
+    sign = 1;
+  }
+  num = (ch - '0') * sign;
+  while (1) {
+    ch = json_getc(parser);
+    if (ch == JSON_EOF) {
+      break;
+    }
+    if (isdigit(ch)) {
+      if (num == 0) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "leading zeros are not allowed");
+      }
+      d = (ch - '0') * sign;
+      if (num < MRB_INT_MIN / 10 ||
+          (num == MRB_INT_MIN / 10 && d < MRB_INT_MIN - num * 10) ||
+          num > MRB_INT_MAX / 10 ||
+          (num == MRB_INT_MAX / 10 && d > MRB_INT_MAX - num * 10)) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "integer overflow");
+        return -1;
+      }
+      num = num * 10 + d;
+    } else if (ch == '.' || ch == 'e' || ch == 'E') {
+      return json_parse_number2(parser, ch, result, num, sign);
+    } else if (json_delimiter_p(ch)) {
+      json_ungetc(parser);
+      break;
+    } else {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "invalid number");
+    }
+  }
+  *result = mrb_fixnum_value(num);
+  return 1;
+}
+
+static int
+json_parse_number2(struct json_parser *parser, int ch, mrb_value *result, int num, int sign)
+{
+  mrb_state *mrb = parser->mrb;
+  double d;
+  int i, state;
+  char buf[64];
+
+  /*
+   * "-"? ("0" | [1-9] digit* ) ("." digit+ )? ([eE][-+] digit+)?
+   * state:                      000 111111     22223333 444444
+   */
+  i = snprintf(buf, sizeof(buf), "%s%d%c",
+      (num == 0 && sign < 0) ? "-" : "",
+      num, ch);
+  if (ch == '.')
+    state = 0;
+  else /* (ch == 'e' || ch == 'E') */
+    state = 2;
+  while (1) {
+    ch = json_getc(parser);
+    if (ch == JSON_EOF)
+      break;
+    switch (state) {
+      case 0:
+        if (!isdigit(ch))
+          goto formaterr;
+        state = 1;
+        break;
+      case 1:
+        if (isdigit(ch))
+          ; /* read more digits */
+        else if (ch == 'e' || ch == 'E')
+          state = 2;
+        else if (json_delimiter_p(ch)) {
+          json_ungetc(parser);
+          state = -1;
+        } else
+          goto formaterr;
+        break;
+      case 2:
+        if (ch == '-' || ch == '+')
+          state = 3;
+        else if (isdigit(ch))
+          state = 4;
+        else
+          goto formaterr;
+        break;
+      case 3:
+        if (!isdigit(ch))
+          goto formaterr;
+        state = 4;
+        break;
+      case 4:
+      default:
+        if (isdigit(ch))
+          ; /* read more digits */
+        else {
+          json_ungetc(parser);
+          state = -1;
+        }
+        break;
+    }
+    if (state == -1)
+      break;
+    if (i == sizeof(buf) - 1) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "floating point number too long");
+    }
+    buf[i++] = ch;
+  }
+  buf[i] = '\0';
+  sscanf(buf, "%lf", &d);
+  *result = mrb_float_value(mrb, d);
+  return 1;
+
+formaterr:
+  mrb_raise(mrb, E_JSON_PARSER_ERROR, "floating point number error");
+  return -1;
+}
+
+static int
+json_parse_string(struct json_parser *parser, mrb_value *result)
+{
+  mrb_state *mrb = parser->mrb;
+  mrb_value str;
+  int ch, n;
+  char *cp;
+
+  str = mrb_str_buf_new(mrb, 30);
+  cp = RSTRING_PTR(str);
+  n = 0;
+
+  while (1) {
+    ch = json_getc(parser);
+    if (ch == JSON_EOF) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "EOF in string");
+    }
+    
+    if (ch == '"') {
+      break;
+    } else if (ch == '\\') {
+      ch = json_getc(parser);
+      if (ch == JSON_EOF) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "EOF following escape char");
+      }
+      switch (ch) {
+        case '"':
+        case '\\':
+        case '/':
+          break;
+        case 'b':
+          ch = 0x08;
+          break;
+        case 'f':
+          ch = 0x0c;
+          break;
+        case 'n':
+          ch = 0x0a;
+          break;
+        case 'r':
+          ch = 0x0d;
+          break;
+        case 't':
+          ch = 0x09;
+          break;
+        case 'u':
+          /* XXX: Unicode is not supported */
+          (void)json_getc(parser);
+          (void)json_getc(parser);
+          (void)json_getc(parser);
+          (void)json_getc(parser);
+          ch = '=';
+          break;
+        default:
+          mrb_raise(mrb, E_JSON_PARSER_ERROR, "invalid escape char");
+          break;
+      }
+    } else if (ch < 0x20 || ch > 0x7e) {
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "invalid char");
+    }
+
+    if (n + 1 == RSTRING_CAPA(str)) {
+      mrb_str_resize(mrb, str, RSTRING_CAPA(str)*2);
+      cp = RSTRING_PTR(str);
+    }
+    cp[n++] = ch;
+  }
+  cp[n] = '\0';
+  mrb_str_resize(mrb, str, n);
+  *result = str;
+  return 1;
+}
+
+static int
+json_parse_value(struct json_parser *parser, mrb_value *result)
+{
+  mrb_state *mrb = parser->mrb;
+  int ch;
+
+  do {
+    ch = json_getc(parser);
+    if (ch == JSON_EOF)
+      return 0;
+  } while (json_whitespace_p(ch));
+
+  switch (ch) {
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
+    case '-':
+      if (json_parse_number(parser, ch, result) != 1) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "number...?");
+      }
+      break;
+
+    case '"':
+      if (json_parse_string(parser, result) != 1) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "string...?");
+      }
+      break;
+
+    case '[':
+      if (json_parse_array(parser, result) != 1) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "array...?");
+      }
+      break;
+
+    case '{':
+      if (json_parse_object(parser, result) != 1) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "object...?");
+      }
+      break;
+
+    case 'f':
+      if (json_parse_readstring(parser, "alse") != 1) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "unexpected f");
+      }
+      *result = mrb_false_value();
+      break;
+
+    case 'n':
+      if (json_parse_readstring(parser, "ull") != 1) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "unexpected n");
+      }
+      *result = mrb_nil_value();
+      break;
+
+    case 't':
+      if (json_parse_readstring(parser, "rue") != 1) {
+        mrb_raise(mrb, E_JSON_PARSER_ERROR, "unexpected t");
+      }
+      *result = mrb_true_value();
+      break;
+
+    default:
+      mrb_raise(mrb, E_JSON_PARSER_ERROR, "unexpected character");
+  }
+
+  return 1;
+}
+
+static mrb_value
+mrb_json_parse(mrb_state *mrb, mrb_value mod)
+{
+  struct json_parser parser;
+  mrb_value obj, options, source;
+
+  mrb_get_args(mrb, "S|H", &source, &options);
+
+  parser.mrb         = mrb;
+  parser.src         = source;
+  parser.cursor      = 0;
+  parser.lineno      = 0;
+  parser.nesting     = 0;
+  parser.max_nesting = DEFAULT_MAX_NESTING;
+
+  json_parse_value(&parser, &obj);
+
+  // if we have extra characters:
+  // unexpected token at '3' (JSON::ParserError)
+
+  return obj;
+}
+
+void
+mrb_mruby_iijson_gem_init(mrb_state *mrb)
+{
+  struct RClass *m;
+
+  m = mrb_define_module(mrb, "JSON");
+  mrb_define_module_function(mrb, m, "parse", mrb_json_parse, MRB_ARGS_REQ(1)|MRB_ARGS_OPT(1));
+}
+
+void
+mrb_mruby_iijson_gem_final(mrb_state *mrb)
+{
+}
